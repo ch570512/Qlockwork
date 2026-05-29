d42f29728e4e2f7a5bad323342ea3a1216ec927b
ch570512 <24852492+ch570512@users.noreply.github.com>
2026-05-29 10:13:37 +0200

    fixed moonphase

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -23,31 +23,6 @@ uint8_t getMinute(time_t zeit)
     return (zeit % 3600) / 60;
 }
 
-// // Calculate moonphase
-// int getMoonphase(int y, int m, int d)
-// {
-//     int b = 0;
-//     int c = 0;
-//     int e = 0;
-//     double jd = 0;
-
-//     if (m < 3)
-//     {
-//         y--;
-//         m += 12;
-//     }
-//     ++m;
-//     c = 365.25 * y;
-//     e = 30.6 * m;
-//     jd = c + e + d - 694039.09; // jd is total days elapsed
-//     jd /= 29.53;                // divide by the moon cycle (29.53 days)
-//     b = jd;                     // int(jd) -> b, take integer part of jd
-//     jd -= b;                    // subtract integer part to leave fractional part of original jd
-//     b = jd * 8 + 0.5;           // scale fraction from 0-8 and round by adding 0.5
-//     b = b & 7;                  // 0 and 8 are the same so turn 8 into 0
-//     return b;
-// }
-
 // Calculate moonphase
 int getMoonphase(int y, int m, int d) {
     // Use double for everything to prevent precision loss/overflow
